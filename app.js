54d3f52e843c95f59c015ebe6bd0c3cb3fb59d56
Pachara <pachara@getlinks.co>
2017-11-22 17:56:27 +0700

    aaa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World JAMESKUNG WTF!!!!!!!'))
+app.get('/', (req, res) => res.send({data: 'Hello World JAMESKUNG WTF!!!!!!!'}))
 
 app.listen(3333, () => console.log('Example app listening on port 3333!'))
